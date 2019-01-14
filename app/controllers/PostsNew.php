dae50091780ae89bef55ff9b80af1d0635c89202
Kyrylo MIESHKOV <kmieshko@e1r6p11.unit.ua>
2019-01-14 19:43:52 +0200

    lesson 3

---
+++ app/controllers/PostsNew.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\controllers;
+
+class PostsNew {
+
+    public function indexAction() {
+        echo 'PostsNew::index';
+    }
+
+    public function testAction() {
+        echo 'PostsNew::test';
+    }
+
+    public function testPageAction() {
+        echo 'PostsNew::testPage';
+    }
+
+    protected function before() {
+        echo 'PostsNew::before';
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,19 +1,40 @@
 <?php
+ini_set("display_errors", "1");
+error_reporting(-1);
 
-require '../vendor/core/router.php';
+use vendor\core\Router;
+
+$query = rtrim($_SERVER['QUERY_STRING'], '/');
+
+define('WWW', __DIR__);
+define('CORE', dirname(__DIR__).'/vendor/core');
+define('ROOT', dirname(__DIR__));
+define('APP', dirname(__DIR__).'/app');
+
 require '../vendor/libs/functions.php';
 
-$query = $_SERVER['QUERY_STRING'];
+spl_autoload_register(function($class){
+    $file = ROOT.'/'.str_replace('\\', '/', $class).'.php';
+    // $file = APP."/controllers/$class.php";
+    if (is_file($file)) {
+        require_once $file;
+    }
+});
 
-Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
-Router::add('posts/', ['controller' => 'Posts', 'action' => 'index']);
-Router::add('', ['controller' => 'Main', 'action' => 'index']);
+Router::add('^page\/?(?P<action>[a-z-]+)\/(?P<alias>[a-z-]+)$', ['controller' => 'Page']);
+Router::add('^page\/(?P<alias>[a-z-]+)$', ['controller' => 'Page', 'action' => 'view']);
+
+// defaults routs
+Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
+Router::add('^(?P<controller>[a-z-]+)\/?(?P<action>[a-z-]+)?$');
 
 debug(Router::getRoutes());
 
-if (Router::matchRoute($query)) {
-    debug(Router::getRoute());
-}
-else {
-    echo '404';
-}
+// if (Router::matchRoute($query)) {
+//     debug(Router::getRoute());
+// }
+// else {
+//     echo '404';
+// }
+
+Router::dispatch($query);
